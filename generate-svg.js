846c6768fa7b266afd571ba967296e14a4c690d4
zoushijun <zoushijun@threatbook.cn>
2017-10-27 17:27:10 +0800

    modify

--- generate-svg.js
+++ generate-svg.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 var result = glob.sync('./svg/*.svg');
 var t = result.map((item) => {
   var content = fs.readFileSync(item).toString();
-  return '<div>' + content + '</div><div>' + item.replace('./svg/', '') + '</div>';
+  return '<div class="left">' + content + '</div><div class="right">' + item.replace('./svg/', '') + '</div>';
 }).join('');
 fs.writeFileSync('./test/svg.ejs', t);
