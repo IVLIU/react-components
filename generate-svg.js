4705da495fdb2aa8db3eef65f3dcd009541cf0f6
sunxiaoshen <sunxiaoshen@threatbook.cn>
2017-10-27 17:02:50 +0800

    add svg html

---
+++ generate-svg.js
@@ -0,0 +1,8 @@
+var glob = require('glob');
+var fs = require('fs');
+var result = glob.sync('./svg/*.svg');
+var t = result.map((item) => {
+  var content = fs.readFileSync(item).toString();
+  return '<div>' + content + '</div><div>' + item.replace('./svg/', '') + '</div>';
+}).join('');
+fs.writeFileSync('./svg.html', t);
