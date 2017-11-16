261d013f0342160c74771446ba838f9762d1ec61
sunxiaoshen <sunxiaoshen@threatbook.cn>
2017-11-16 17:22:09 +0800

    fix

--- server.js
+++
@@ -1,9 +0,0 @@
-var static = require('node-static');
-var file = new static.Server('./uiguide');
-
-
-require('http').createServer(function (request, response) {
-    request.addListener('end', function () {
-        file.serve(request, response);
-    }).resume();
-}).listen(9999);
