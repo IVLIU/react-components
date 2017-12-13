08d49fedfa22e62926fea75b1148c7d1bb5455d2
sunxiaoshen <sunxiaoshen@threatbook.cn>
2017-12-13 16:52:16 +0800

    add menu-mini class

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var bulkSass = require('gulp-sass-bulk-import');
+var gulpSequence = require('gulp-sequence')
+
  
 gulp.task('css', function() {
 	return gulp
@@ -16,7 +18,18 @@ gulp.task('css', function() {
 });
 
 gulp.task('css:watch', function() {
-  gulp.watch('./styles/modules/*.scss', ['css']);
+  gulp.watch('./styles/modules/*.scss', function(cb) {
+    gulpSequence('css', 'copy')(function(err) {
+      if (err) {
+        console.log(err);
+      }
+    })
+  });
+});
+
+gulp.task('copy', function() {
+  return gulp.src('./dist/index.css')
+    .pipe(gulp.dest('../tip/src/styles/'));
 });
  
 gulp.task('sass', function () {
