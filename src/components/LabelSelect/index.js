f6d47019529ff387d0667056e21b8b1366fa7dea
callmedadaxin <554091944@qq.com>
2018-02-02 10:58:46 +0800

    fix labelselect

--- src/components/LabelSelect/index.js
+++ src/components/LabelSelect/index.js
@@ -15,6 +15,21 @@ export default class LabelSelect extends Component {
       value: defaultValue || multi ? [] : options[0].value
     }
   }
+  componentWillReceiveProps (nextProps) {
+    const { defaultValue, value } = nextProps
+    const { defaultValue: curDefault } = this.props
+    if (curDefault !== defaultValue) {
+      this.setState({
+        value: defaultValue
+      })
+      return
+    }
+    if (value !== undefined) {
+      this.setState({
+        value
+      })
+    }
+  }
   isAllSelected (value) {
     const { options } = this.props
     return value.length === options.length
@@ -60,7 +75,7 @@ export default class LabelSelect extends Component {
     const { className, options, multi, showAll, ...others } = this.props
     const classes = classNames('label-select', className)
     const { value } = this.state
-    const isAllActive = !value.length && multi
+    const isAllActive = multi && !value.length
     return (
       <div className={classes} {...others}>
         {
