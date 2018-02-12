a0d3cca9fe333dd8cb7d53ca49531bf6dc988a09
zoushijun <zoushijun@threatbook.cn>
2018-02-12 18:26:22 +0800

    modify PageTitle/index.js

--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -2,16 +2,17 @@
  * @Author: zsj
  * @Date: 2018-02-12 15:58:08
  * @Last Modified by: zsj
- * @Last Modified time: 2018-02-12 16:40:12
+ * @Last Modified time: 2018-02-12 17:51:15
  */
 
 import React, { Component } from 'react'
 import { string } from 'prop-types'
 import classNames from 'classnames'
 
-/*
-  每页的标题
+/**
+ * 页面头部标题
  */
+
 class PageTitle extends Component {
   static propTypes = {
     /** 唯一需要传入的属性 */
