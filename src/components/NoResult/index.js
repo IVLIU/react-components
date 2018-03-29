edda4670cb751caf5e1b09bc75340a1bc85947a1
callmedadaxin <554091944@qq.com>
2018-03-29 15:54:56 +0800

    add no result

---
+++ src/components/NoResult/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import classNames from 'classnames'
+
+export default class NoResult extends Component {
+  static propTypes = {
+    desc: PropTypes.string
+  }
+  static defaultProps = {
+    desc: '暂无数据'
+  }
+
+  render () {
+    const { desc, className, ...others } = this.props
+    const classes = classNames('no-result', className)
+    return (
+      <div className={classes} {...others}>
+        {desc}
+      </div>
+    )
+  }
+}
