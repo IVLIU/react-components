06caa9890604522f9246a3ae75a426d8e610247a
callmedadaxin <554091944@qq.com>
2018-04-17 17:04:22 +0800

    fix radio

--- src/components/Radio/index.js
+++ src/components/Radio/index.js
@@ -1,6 +1,4 @@
 import Radio from './Radio'
 import RadioGroup from './RadioGroup.js'
-import RadioBtn from './RadioButton'
 Radio.RadioGroup = RadioGroup
-Radio.RadioBtn = RadioBtn
 export default Radio
