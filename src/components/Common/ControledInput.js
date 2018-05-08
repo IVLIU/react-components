72548c71c1ddf842fedc012b22d4a36aebc704a4
callmedadaxin <554091944@qq.com>
2018-05-08 20:43:32 +0800

    fix controled

--- src/components/Common/ControledInput.js
+++ src/components/Common/ControledInput.js
@@ -2,7 +2,7 @@
  * @Author: wangweixin@threatbook.cn
  * @Date: 2018-01-18 17:51:37
  * @Last Modified by: wangweixin@threatbook.cn
- * @Last Modified time: 2018-05-07 11:35:48
+ * @Last Modified time: 2018-05-07 14:13:43
  */
 import React, { Component } from 'react'
 import pureRender from 'pure-render-decorator'
@@ -11,14 +11,14 @@ import autobind from 'autobind-decorator'
 const controledInput = (WrapComponent, mapDefaultToValue, MapValueToValue) => {
   @pureRender
   class RetComponent extends Component {
-    constructor(props) {
+    constructor (props) {
       super(props)
       const { defaultValue } = props
       this.state = {
         value: mapDefaultToValue(defaultValue, props)
       }
     }
-    componentWillReceiveProps(nextProps) {
+    componentWillReceiveProps (nextProps) {
       const { defaultValue } = this.props
       const { defaultValue: d } = nextProps
       if (d !== defaultValue) {
@@ -28,7 +28,7 @@ const controledInput = (WrapComponent, mapDefaultToValue, MapValueToValue) => {
       }
     }
     @autobind
-    handleChange(value, ...others) {
+    handleChange (value, ...others) {
       const { onChange } = this.props
       const ret = MapValueToValue(value, this.props, ...others)
       this.setState({
@@ -36,7 +36,7 @@ const controledInput = (WrapComponent, mapDefaultToValue, MapValueToValue) => {
       })
       onChange && onChange(ret)
     }
-    render() {
+    render () {
       let defaultProps = Object.assign({}, this.props)
       const props = {
         value: this.state.value,
