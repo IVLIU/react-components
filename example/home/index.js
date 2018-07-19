487e8dbe28771c33cb32793ac29347fc05d7d255
callmedadaxin <554091944@qq.com>
2018-07-19 15:17:03 +0800

    fix pagination

--- example/home/index.js
+++ example/home/index.js
@@ -463,7 +463,7 @@ export default class Home extends Component {
         <LabelSelect onChange={console.log} defaultValue={2} options={listItems}/>
         <LabelSelect onChange={console.log} disabled defaultValue={[2, 3]} options={listItems} multi/>
         <LabelSelect onChange={console.log} showAll={false} options={listItems} multi/>
-        <Pagination current={current} total={25} onChange={this.handlePageChange.bind(this)}/>
+        <Pagination current={current} total={250} onChange={this.handlePageChange.bind(this)}/>
         <Loading size="lg"/>
         <Loading/>
         <Loading type="box">正在提交...</Loading>
